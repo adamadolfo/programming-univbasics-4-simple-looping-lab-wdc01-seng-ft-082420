fa3d53d7dc6a018173c4f8d85801c5c5a8a0835b
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-07 00:22:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,8 +18,10 @@ def output_array(array)
 end
 def return_string_array(array)
   counter = 0 
+  new_array = []
   while counter < array.length do
     array[counter].to_s
   counter += 1
   end
+  new_array
 end
